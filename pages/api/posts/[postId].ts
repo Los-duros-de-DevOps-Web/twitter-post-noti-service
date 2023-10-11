dfd8a7e883d87b49f5fd0f09bc0a5291027a3ff4
GabrielSB19 <gasuarez2002@gmail.com>
2023-10-11 15:52:25 -0500

    Added the post and notification service

--- pages/api/posts/[postId].ts
+++ pages/api/posts/[postId].ts
@@ -2,16 +2,19 @@ import { NextApiRequest, NextApiResponse } from "next";
 
 import prisma from "@/libs/prismadb";
 
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-  if (req.method !== 'GET') {
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  if (req.method !== "GET") {
     return res.status(405).end();
   }
 
   try {
     const { postId } = req.query;
 
-    if (!postId || typeof postId !== 'string') {
-      throw new Error('Invalid ID');
+    if (!postId || typeof postId !== "string") {
+      throw new Error("Invalid ID");
     }
 
     const post = await prisma.post.findUnique({
@@ -22,11 +25,11 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
         user: true,
         comments: {
           include: {
-            user: true
+            user: true,
           },
           orderBy: {
-            createdAt: 'desc'
-          }
+            createdAt: "desc",
+          },
         },
       },
     });
